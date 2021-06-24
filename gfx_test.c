c53d838b7185c58757f230c2e0d89d295deca819
Mike Klein <mtklein@gmail.com>
2021-06-24 11:58:06 -0400

    test store_rgba_f16

--- gfx_test.c
+++ gfx_test.c
@@ -5,7 +5,7 @@ static const Slab empty = {0};
 static const F32  zero  = {0};
 
 static void test_load_rgba_f16() {
-    _Float16 px[32] = { 0.0f16, 0.25f16, 0.5f16, 0.75f16, 1.0f16 };
+    _Float16 px[4*N] = { 0.0f16, 0.25f16, 0.5f16, 0.75f16, 1.0f16 };
     Slab s = load_rgba_f16(px, empty,empty, zero,zero);
 
     expect(s.r[0] == 0.00f16);
@@ -15,8 +15,25 @@ static void test_load_rgba_f16() {
     expect(s.r[1] == 1.00f16);
 }
 
+static void test_store_rgba_f16() {
+    Slab src = {
+        {0.00f16, 1.0f16},
+        {0.25f16},
+        {0.50f16},
+        {0.75f16},
+    };
+
+    _Float16 px[4*N] = {0};
+    store_rgba_f16(px, src,empty,zero,zero);
+    expect(px[0] == 0.00f16);
+    expect(px[1] == 0.25f16);
+    expect(px[2] == 0.50f16);
+    expect(px[3] == 0.75f16);
+    expect(px[4] == 1.00f16);
+}
+
 static void test_load_rgba_unorm8() {
-    uint8_t px[32] = { 0x00, 0x55, 0xaa, 0xfe, 0xff };
+    uint8_t px[4*N] = { 0x00, 0x55, 0xaa, 0xfe, 0xff };
     Slab s = load_rgba_unorm8(px, empty,empty, zero,zero);
 
     expect(s.r[0] == 0.0f16);
@@ -27,7 +44,7 @@ static void test_load_rgba_unorm8() {
 }
 
 static void test_load_rgba_unorm16() {
-    uint16_t px[32] = { 0x0000, 0x5555, 0xaaaa, 0xffee, 0xffff };
+    uint16_t px[4*N] = { 0x0000, 0x5555, 0xaaaa, 0xffee, 0xffff };
     Slab s = load_rgba_unorm16(px, empty,empty, zero,zero);
 
     expect(s.r[0] == 0.0f16);
@@ -39,6 +56,7 @@ static void test_load_rgba_unorm16() {
 
 int main(void) {
     test_load_rgba_f16();
+    test_store_rgba_f16();
     test_load_rgba_unorm8();
     test_load_rgba_unorm16();
     return 0;
