2fea79c918337ea124594958eb302f9ecfc7a5d3
Mike Klein <mtklein@gmail.com>
2021-07-22 10:37:45 -0500

    refactoring

--- murmur3.c
+++ murmur3.c
@@ -12,19 +12,22 @@ uint32_t mix(uint32_t hash) {
 }
 
 __attribute__((no_sanitize("unsigned-integer-overflow")))
-uint32_t murmur3(uint32_t hash, const void* vptr, const size_t len) {
-    const char* ptr = vptr;
-    size_t bytes = len;
+static uint32_t mixA(uint32_t v) {
+    v *= 0xcc9e2d51;
+    v = (v<<15) | (v>>17);
+    v *= 0x1b873593;
+    return v;
+}
 
-    for (; bytes >= 4; bytes -= 4, ptr += 4) {
+__attribute__((no_sanitize("unsigned-integer-overflow")))
+uint32_t murmur3(uint32_t hash, const void* vptr, const size_t bytes) {
+    const char* ptr = vptr;
+    size_t      len = bytes;
+
+    for (; len >= 4; len -= 4, ptr += 4) {
         uint32_t v;
         memcpy(&v, ptr, sizeof v);
-
-        v *= 0xcc9e2d51;
-        v = (v<<15) | (v>>17);
-        v *= 0x1b873593;
-
-        hash ^= v;
+        hash ^= mixA(v);
 
         hash = (hash<<13) | (hash>>19);
         hash *= 5;
@@ -32,17 +35,11 @@ uint32_t murmur3(uint32_t hash, const void* vptr, const size_t len) {
     }
 
     uint32_t v = 0;
-    switch (bytes) {
+    switch (len) {
         case 3: v |= (uint32_t)ptr[2] << 16;
         case 2: v |= (uint32_t)ptr[1] <<  8;
         case 1: v |= (uint32_t)ptr[0] <<  0;
-                v *= 0xcc9e2d51;
-                v = (v<<15) | (v>>17);
-                v *= 0x1b873593;
-
-                hash ^= v;
+                hash ^= mixA(v);
     }
-
-    hash ^= (uint32_t)len;
-    return mix(hash);
+    return mix(hash ^ (uint32_t)bytes);
 }
