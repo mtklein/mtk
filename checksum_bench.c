e3b4621bac5672f2b7044b3cf8e4692ce44c05ab
Mike Klein <mtklein@gmail.com>
2021-07-28 10:01:20 -0500

    add some comparison checksums

--- checksum_bench.c
+++ checksum_bench.c
@@ -4,6 +4,34 @@
 
 static uint32_t seed = 0;
 
+static double bench_crc32(int k, double *scale, const char* *unit) {
+    char buf[1024] = {0};
+    memcpy(buf, &seed, sizeof seed);
+
+    *scale = sizeof buf;
+    *unit  = "B";
+
+    double start = now();
+    while (k --> 0) {
+        expect_eq(0, crc32(0, buf,sizeof buf));
+    }
+    return now() - start;
+}
+
+static double bench_fnv1a(int k, double *scale, const char* *unit) {
+    char buf[1024] = {0};
+    memcpy(buf, &seed, sizeof seed);
+
+    *scale = sizeof buf;
+    *unit  = "B";
+
+    double start = now();
+    while (k --> 0) {
+        expect_eq(0x1f116dc5, fnv1a(buf,sizeof buf));
+    }
+    return now() - start;
+}
+
 static double bench_mix(int k, double *scale, const char* *unit) {
     *scale = sizeof seed;
     *unit  = "B";
@@ -34,6 +62,8 @@ int main(int argc, char** argv) {
         seed = 42;
     }
 
+    bench(bench_crc32);
+    bench(bench_fnv1a);
     bench(bench_mix);
     bench(bench_murmur3);
     return 0;
