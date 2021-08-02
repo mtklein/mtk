9c80fd139aa10ba1f40e5e02433e78618b8ebeff
Mike Klein <mtklein@gmail.com>
2021-08-02 08:55:43 -0500

    simplify

--- test.h
+++ test.h
@@ -33,16 +33,6 @@ static inline double now() {
     return clock() * (1.0 / CLOCKS_PER_SEC);
 }
 
-static void print_rate(double rate) {
-    const char* units[] = { "", "k", "M", "G", "T" };
-    const char* *unit = units;
-    while (rate > 1000) {
-        rate /= 1000;
-        unit++;
-    }
-    printf("%.3g%s", rate, *unit);
-}
-
 static inline void bench_(const char* name, double(*fn)(int, double*, const char**)) {
     double      scale = 1.0;
     const char* unit  = "";
@@ -60,9 +50,15 @@ static inline void bench_(const char* name, double(*fn)(int, double*, const char
         elapsed = fn(k, &scale,&unit);
     }
 
-    printf("%-30s", name);
-    print_rate(k/elapsed * scale);
-    printf("%s\n", unit);
+    double rate = k/elapsed * scale;
+    const char* powers[] = { "", "k", "M", "G", "T" };
+    const char* *power = powers;
+    while (rate > 1000) {
+        rate /= 1000;
+        power++;
+    }
+
+    printf("%-30s\t%.3g\t%s%s\n", name,rate,*power,unit);
 }
 
 #define bench(fn) if (argc == 1 || 0 == strcmp(argv[1], #fn)) bench_(#fn,fn)
