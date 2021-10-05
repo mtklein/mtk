3e5960a4a1450be3e12ca789f98cf512d94a7e94
Mike Klein <mtklein@gmail.com>
2021-10-05 08:54:07 -0500

    yeah insertion sort is better, 1.69->2.47

--- sort.c
+++ sort.c
@@ -2,8 +2,8 @@
 
 void sort(int arr[], int n) {
     for (int *i = arr; i < arr+n; i++)
-    for (int *j = arr; j < arr+n; j++) {
-        if (*i < *j) {
+    for (int *j = i+1; j < arr+n; j++) {
+        if (*i > *j) {
             int tmp = *i;
             *i = *j;
             *j = tmp;
